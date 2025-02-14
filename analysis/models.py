0a49b8a07c695be806e63b28676e0f457ef6c291
shathaalharbi9 <tu4250576@taibahu.edu.sa>
2025-02-14 14:31:58 +0300

    edit in SQL

--- analysis/models.py
+++ analysis/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+#from multiagent.models import MultiAgent
 
-# Create your models here.
+class Analysis(models.Model):
+    model_id = models.AutoField(primary_key=True )
+    analyzedPhoto = models.TextField(default="No photo analyzed.")
+    analyzedText = models.TextField(default="No text analyzed.")
+    analyzedVideo = models.TextField(default="No video analyzed.")
+    analyzedWebUsage = models.TextField(default="No web usage analyzed.")
+    #multiAgent_id = models.ForeignKey(MultiAgent, on_delete=models.CASCADE, null=True, blank=True )
